f91e9cb2d4d7988061325a1412b03707148af36e
muonroi <leanhphi1706@gmail.com>
2024-03-05 22:16:31 +0700

    Complete third screen onboarding

---
+++ config.js
@@ -0,0 +1,47 @@
+var config = {
+    texts: [
+        "Anh biết trong thời gian qua",      
+        "Anh đã rất vô tâm đối với em....",
+        "Nhưng anh sẽ...",
+        "THAY ĐỔI VÌ EM",
+        "Vì sao lại thay đổi ư?",
+        "--> Đơn giản là vì anh yêu em ^^ <--",
+        "Em có yêu anh hông?",
+        "SUỴTTT!!!",
+        "Trước khi em noái hông",
+        "Hì để anh noái trước",
+        "Anh thật sự",
+        "Rất rất là YÊU EM!!!!!!!!!!",
+        "CHÚC VỢ ANH VÀO NGÀY 8-3",
+        "Luôn luôn xinh đẹp",
+        "Luôn luôn vui vẻ",
+        "Luôn luôn hạnh phúc",
+        "Và yêu anh thiệc là nhiềuuuu hí hí!!!",
+        "Anh yêu em nhiều lắm óooooo",
+    ],
+    imgs: {
+        "送给我": "./imgs/songxinxin.jpeg",
+        "心爱的姑娘": "./imgs/nothing.jpg",
+        "今天是你的生日": "./imgs/shengri.jpeg",
+        "这两年来，我见过你": "./imgs/yangzi.jpeg",
+        "奇怪的样子": "./imgs/suyt.jpg",
+        "可爱的样子": "./imgs/before.jpg",
+        "生气的样子": "./imgs/ask.png",
+        "都是我喜欢的样子": "./imgs/after.png",
+        "对了，礼物收全了吗？": "./imgs/chuo.gif",
+        "今年要吃好好的": "./imgs/end.png",
+    },
+    
+    desc: {
+        turn_on: "Vợ bấm vô đây nà",
+        play: "Lơn nhạc",
+        bannar_coming: "Thêm tí màu choa đẹp nhò",
+        balloons_flying: "Chưa đủ mood đúng hemmmmm",
+        cake_fadein: "Bánh kem choa ngày 8/3 nà",
+        light_candle: "Thắp nến choa lãng mạn nhó",
+        wish_message: "Chúc vợ ngày 8/3 thiệc thiệc là dui dẻ nhó choyyyyyyyyyyyy",
+        story: "Anh yêu em nhiều lắm đoáaaaaaaaaa"
+    },
+
+    loveText: 'Yêu vợ Hân nhứtttttt~'
+};
